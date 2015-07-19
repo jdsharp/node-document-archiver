9d873372166233d7ba68d371e7eb9be0f15b613a
Jonathan Sharp <jonathan@jdsharp.com>
2015-07-18 19:56:46 -0500

    Adding example config file

---
+++ examples/receipts/config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = [
+  { rule    : 'My Receipts',
+    enabled : true, // Defaults to true
+    src     : path.join(__dirname, 'example/inbox/**/*'),
+    match   : [
+      // Require the file start with a YYYY-MM-DD prefix
+      { test : 'file-prefix-fulldate' },
+
+      // Must have the tag 'RCPT'
+      { test : 'tags', required : ['RCPT'] },
+
+      // Must end with the file extension
+      { test : 'file-ext', ext : ['txt','pdf','jpg','png'] }
+    ],
+    run : [
+      { action : 'normalize-file' },
+      { action : 'categorize', category : 'John Doe' },
+      { action : 'tags', add : ['TEST'] },
+      { action : 'copy', dest : path.join(__dirname, 'example/archived/documents/') },
+      { action : 'folders-by-year' },
+    ]
+  }
+];
